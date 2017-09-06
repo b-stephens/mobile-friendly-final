07d4fe60eb94c91ef87d2c1691232f4111375bc3
Bryan Stephens <Bryan@CodedSolutions.io>
2017-09-06 13:17:29 -0700

    added Fresno skyline to parallax section

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -6,7 +6,7 @@
 $contact_email_to = "";
 
 // Subject prefix
-$contact_subject_prefix = "Contat Form Message: ";
+$contact_subject_prefix = "Contact Form Message: ";
 
 // Name too short error text
 $contact_error_name = "Name is too short or empty!";
